62a815a3b3395563f6e52a004f6130fc92fc968d
vadimsusername <vadimburyakovsky@yahoo.com>
2020-02-10 21:33:42 -0500

    added some questions and event listeners to the answer choices

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 var startView = document.querySelector(".start-view");
-var questionView = document.querySelector(".questions");
 var startBtn = document.querySelector(".start-button");
-
+var question1 = document.querySelector("question1");
 
 startBtn.addEventListener("click",function(){
     startView.style.display = "none";
-    questionView.style.display = "block";
+    question1.style.display = "block";
     
 })
+
+var questions = [".question1",".question2",".question3"];
+var questionIndex = 0;
+
+document.querySelector(".questions").addEventListener("click", function(event){
+
+     if(event.target.matches("button")){
+
+        document.querySelector(questions[questionIndex]).style.display = "none";
+        if(questionIndex === questions.length - 1){
+
+            document.getElementById("quiz-end").style.display = "block";
+            
+        }else{
+            questionIndex++;
+            document.querySelector(questions[questionIndex]).style.display = "block";
+            
+        }            
+        
+     }
+ 
+ 
+ })
