b2958cf119b2160c6b5e2cb38de91d6da9f40abd
vadimsusername <vadimburyakovsky@yahoo.com>
2020-02-10 21:04:36 -0500

    edited starting view

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var startView = document.querySelector(".start-view");
 var questionView = document.querySelector(".questions");
+var startBtn = document.querySelector(".start-button");
+
+
+startBtn.addEventListener("click",function(){
+    startView.style.display = "none";
+    questionView.style.display = "block";
+    
+})
