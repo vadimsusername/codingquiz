27fc04e718309a02e8d3c7e6bd5f6271df1fd49e
vadimsusername <vadimburyakovsky@yahoo.com>
2020-02-10 22:17:10 -0500

    added score keeping

--- script.js
+++ script.js
@@ -10,11 +10,27 @@ startBtn.addEventListener("click",function(){
 
 var questions = [".question1",".question2",".question3"];
 var questionIndex = 0;
+var score = 0;
 
 document.querySelector(".questions").addEventListener("click", function(event){
 
      if(event.target.matches("button")){
 
+        
+        if(event.target.matches(".correct")){
+
+            console.log("answer is correct");
+            score = score + 5;
+            var response = document.createElement("h3");
+            response.textContent ="Correct";
+            console.log(questions[questionIndex]);
+            document.querySelector(questions[questionIndex]).appendChild(response);
+            
+        }else{
+            var response = document.createElement("h3");
+            response.textContent ="Incorrect";
+            document.querySelector(questions[questionIndex]).appendChild(response);
+        }            
         document.querySelector(questions[questionIndex]).style.display = "none";
         if(questionIndex === questions.length - 1){
 
@@ -24,8 +40,7 @@ document.querySelector(".questions").addEventListener("click", function(event){
             questionIndex++;
             document.querySelector(questions[questionIndex]).style.display = "block";
             
-        }            
-        
+        }
      }
  
  
