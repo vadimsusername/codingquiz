738585777f06b451c45dd9aa2483105493a20d7a
vadimsusername <vadimburyakovsky@yahoo.com>
2020-02-12 23:42:23 -0500

    added highscores page, updated index.html

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var startView = document.querySelector(".start-view");
 var startBtn = document.querySelector(".start-button");
-var question1 = document.querySelector("question1");
+console.log(startBtn);
+var startView = document.querySelector(".start-view");
+console.log(startView);
+var q1element = document.querySelector(".question1");
+console.log(q1element);
 
+var questions = [".question1",".question2",".question3",".question4",".question5"];
+var questionIndex =0;
+var score = 0;
 var timer = 20;
 var timeInterval;
 
+function quizOver(){
+    document.getElementById("quiz-end").style.display = "block";
+    document.getElementById("final-score").textContent = score;
+}
+
 startBtn.addEventListener("click",function(){
     startView.style.display = "none";
-    question1.style.display = "block";
-    timeInterval = setInterval(function(){
+    q1element.style.display = "block";
+    
+     timeInterval = setInterval(function(){
 
         if(timer === 0){
             document.querySelector(questions[questionIndex]).style.display = "none";
@@ -27,18 +39,13 @@ startBtn.addEventListener("click",function(){
             document.getElementById("timer").textContent = timer;
         }
     },1000);
-    
 })
 
-var questions = [".question1",".question2",".question3"];
-var questionIndex = 0;
-var score = 0;
 
 document.querySelector(".questions").addEventListener("click", function(event){
-
-     if(event.target.matches("button")){
-
-        
+   // var ans = event.target.getAttribute("data-answer");
+    //console.log(ans);
+    if(event.target.matches("button")){
         if(event.target.matches(".correct")){
 
             console.log("answer is correct");
@@ -52,18 +59,70 @@ document.querySelector(".questions").addEventListener("click", function(event){
             var response = document.createElement("h3");
             response.textContent ="Incorrect";
             document.querySelector(questions[questionIndex]).appendChild(response);
-        }            
-        document.querySelector(questions[questionIndex]).style.display = "none";
-        if(questionIndex === questions.length - 1){
-
-            document.getElementById("quiz-end").style.display = "block";
-            
-        }else{
-            questionIndex++;
-            document.querySelector(questions[questionIndex]).style.display = "block";
-            
+            timer = timer - 3;
         }
-     }
+        setTimeout(function(){
+            document.querySelector(questions[questionIndex]).style.display = "none";
+            if(questionIndex === questions.length - 1){
+                console.log(document.getElementById("quiz-end"));
+
+                clearInterval(timeInterval);
+
+                document.getElementById("final-score").textContent = score;
+
+                document.getElementById("quiz-end").style.display = "block";
+                
+            }else{
+                questionIndex++;
+                document.querySelector(questions[questionIndex]).style.display = "block";
+                
+            }
+        },1000);
+    }
+
+
+})
+
+var highscoresArray = [];
+
+document.getElementById("submit").addEventListener("click",function(event){
+    
+    event.preventDefault();
+    console.log("submit button handler");
+    var initials = document.getElementById("initials").value;
+    var entry = {scr:score,initls: initials};
+    console.log(entry);
+    var storage = JSON.parse(localStorage.getItem("highscores"));
+    console.log(storage);
+    if(!storage){
+        highscoresArray.push(entry);
+        console.log(highscoresArray);
+        console.log(JSON.stringify(highscoresArray));
+        localStorage.setItem("highscores",JSON.stringify(highscoresArray));
+        console.log(localStorage.getItem("highscores"));
+        console.log(JSON.parse(localStorage.getItem("highscores")));
+
+    }else{
+        console.log(JSON.parse(localStorage.getItem("highscores")));
+        highscoresArray = JSON.parse(localStorage.getItem("highscores"));
+        console.log("in else ... highscores = " + highscoresArray);
+        highscoresArray.push(entry);
+        console.log(highscoresArray);
+        localStorage.setItem("highscores",JSON.stringify(highscoresArray));
+    }
  
- 
- })
+    //window.location.href = "highscores.html";
+})
+
+//highscores.html script
+/*
+var scoreBoard = document.getElementById("score-board");
+
+var hsScores = localStorage.getItem("highscores");
+console.log(hsScores);
+if(hsScores){
+    var parsedScores = JSON.parse(hScores);
+    console.log(parsedScores);
+
+}
+*/
